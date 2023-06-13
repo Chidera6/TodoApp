42eb79436fd56cac42d0685f5632b83dec674c79
chidera6 <dictasteil@gmail.com>
2023-06-13 20:22:25 +0100

    feat:Added CORS

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ const routes = require('./routes');
 const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cors = require('cors');
+
 app.use(bodyParser.json())
+//app.use(cors({
+//    origin: ["http://example1.com", /\.example2\.com$/]
+//}))
 app.use('/api', routes);
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))

fd9080e7da28e22348072c1b527211b31a6a1577
chidera6 <dictasteil@gmail.com>
2023-06-13 15:41:47 +0100

    feat:added one to many relationship between user and tasks

--- backend/db/models/task.js
+++ backend/db/models/task.js
@@ -1,7 +1,6 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+import {User} from '.user'; 
+const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
     /**
@@ -11,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Task.belongsTo(models.User, { foreignKey: 'userId' });
     }
 
   }

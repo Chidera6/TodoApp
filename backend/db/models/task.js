f3a6cdcc072cae36c92e9ec9f91d0898dbe52df9
chidera6 <dictasteil@gmail.com>
2023-06-14 02:50:23 +0100

    fix:deleted some migration files,and ran migrations

--- backend/db/models/task.js
+++ backend/db/models/task.js
@@ -1,50 +1,45 @@
 'use strict';
 
-const {Model} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
       Task.belongsTo(models.User, { foreignKey: 'userId' });
     }
-
   }
-  Task.init({
+
+  Task.init(
+    {
       title: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       description: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
       },
       completed: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false,
-          defaultValue:false
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
       },
       created_at: {
-          type: DataTypes.DATE,
-          allowNull: false,
-          defaultValue: DataTypes.NOW,
-
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
       },
       updated_at: {
-          type:DataTypes.DATE,
-          allowNull:false,
-          defaultValue: DataTypes.NOW,
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Task',
+    }
+  );
 
-      }
-    }, 
-    
- {
-    sequelize,
-    modelName: 'Task',
-  });
   return Task;
 };

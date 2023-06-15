17543b562fe32b5177915e7f62be8005e1a87ed1
chidera6 <dictasteil@gmail.com>
2023-06-15 03:05:20 +0100

    feat:login ndpoints and validations worked

--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -5,8 +5,17 @@ const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validations');
 
-
-router.post('/login',
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Please provide a valid email or username.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors
+];
+router.post('/login',validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
     const user = await User.login({ credential, password });
@@ -17,12 +26,11 @@ router.post('/login',
       err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
-
     await setTokenCookie(res, user);
-
     return res.json({user});
-}
+  }
 );
+
 router.delete('/logout',(_req, res) => {
     res.clearCookie('token');
     return res.json({ message: 'success' });

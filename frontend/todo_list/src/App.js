e98d22dffba194ec66dda3fd3f5e5324e0578325
chidera6 <dictasteil@gmail.com>
2023-06-15 10:59:30 +0100

    fix:deleted signup and login files

--- frontend/todo_list/src/App.js
+++ frontend/todo_list/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Login from './login'
+import Signup from './signup'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 function App(){
   return (
-  <div>
-    <Login/>
-  </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />}></Route>
+      <Route path='/signup' element={<Signup />}></Route>
+    </Routes>
+    </BrowserRouter>
+
   )
 }
 export default App;

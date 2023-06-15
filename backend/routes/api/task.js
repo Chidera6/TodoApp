f79f829fded5837140f650de20a92cb4eb358f6c
chidera6 <dictasteil@gmail.com>
2023-06-15 04:40:20 +0100

    feat:task deletion sucessful

--- backend/routes/api/task.js
+++ backend/routes/api/task.js
@@ -18,5 +18,34 @@ router.post('/create', requireAuth, async (req, res) => {
     }
   });
   
-
+  
+  
+ 
+  router.delete('/:taskId', requireAuth, async (req, res) => {
+    const { taskId } = req.params;
+    const userId = req.user.id;
+  
+    try {
+      
+      const task = await Task.findOne({
+        where: {
+          id: taskId,
+          userId: userId,
+        },
+      });
+  
+      if (!task) {
+        return res.status(404).json({ error: 'Task not found' });
+      }
+  
+      
+      await task.destroy();
+  
+      res.json({ message: 'Task deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting task:', error);
+      res.status(500).json({ error: 'Server Error' });
+    }
+  });
+  
 module.exports = router;

0925c36574a2e483c25aaecaf8a4c944c90f8c0e
chidera6 <dictasteil@gmail.com>
2023-07-07 16:04:38 +0100

    feat:frontend initial commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignupFormPage";
-//import TasksContainer from "./components/TaskPage";
-import * as userActions from "./store/user";
-import Navigation from "./components/Navigation";
-import TasksContainer from "./components/TaskPage/TaskContainer";
-
-
 function App() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-  useEffect(() => {
-    dispatch(userActions.restoreUser()).then(() => setIsLoaded(true));
-  }, [dispatch]);
-
   return (
-    <>
-      <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route exact path="/">
-            <TasksContainer/>
-          </Route>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
-    </>
+    <h1>Hello from App</h1>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,37 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
 import './index.css';
 import App from './App';
-import configureStore from './store';
-import { restoreCSRF, csrfFetch } from './store/csrf';
-import * as userActions from './store/user';
-// ... const store = configureStore();
-
-
-const store = configureStore();
-
-  restoreCSRF();
-  window.csrfFetch = csrfFetch;
-  window.store = store;
-  window.userActions = userActions;
-
-
-
-function Root() {
-  return (
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
-  );
-}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
